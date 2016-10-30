87b3501aff3835a26f130e1a232099e60b151760
Izabela Alves <Izabela Alves>
2016-10-29 23:16:44 -0200

    atualizar

--- src/mega/sena/MegaSena.java
+++ src/mega/sena/MegaSena.java
@@ -1,73 +1,52 @@
-
 package mega.sena;
+
 import java.util.Random;
-
-
 
 public class MegaSena {
 
-    public static void main(String[] args) {
-        int m [][] = new int [3000][6];
-       int v[] = new int[65];
-       
-       Random  gerador = new Random ();
-         
-       for (int i=0; i < 3000  ; i++)
-       {
-           for(int j = 1 ; j <= 60 ; j++)
-               v[j] = 0;
-           
-           int j = 0;
-           
-            while(j < 6)   
-            {
-                int aux = gerador.nextInt(60) + 1;
+	private final int m[][] = new int[3000][6];
+	private final int v[] = new int[65];
+	private final Random gerador = new Random();
 
-                if(v[aux] == 0){
+	public void sortear() {
 
-                    m[i][j++] = aux;
-                    v[aux] = 1;
+	}
 
-                }
-            }        
-        }
-       
-       for(int k = 0 ; k < 3000 ; k++){
-            
-           for(int i = 0 ; i < 6 ; i++){
+	public void sortear3000() {
+		// faz o sorteio de 6 digitos e acrescenta a matriz
+		for (int i = 0; i < 3000; i++) {
+			for (int j = 1; j <= 60; j++)
+				v[j] = 0;
+			int j = 0;
+			while (j < 6) {
+				int aux = gerador.nextInt(60) + 1;
+				if (v[aux] == 0) {
+					m[i][j++] = aux;
+					v[aux] = 1;
+				}
+			}
+		}
 
-                for(int j = i ; j > 0 ; j--){
+		// percorre a matriz e ordena os digitos de cada sorteio
+		for (int k = 0; k < 3000; k++) {
+			for (int i = 0; i < 6; i++) {
+				for (int j = i; j > 0; j--) {
+					if (m[k][j] < m[k][j - 1]) {
+						int aux = m[k][j];
+						m[k][j] = m[k][j - 1];
+						m[k][j - 1] = aux;
+					} else
+						break;
+				}
+			}
+		}
 
-                    if(m[k][j] < m[k][j - 1]){
+		// forma elegante de exibir os 3000 sorteios
+		for (int i = 0; i < 3000; i++) {
+			System.out.println("Sorteio [" + (i + 1) + "]: " + m[i][0] + " " + m[i][1] + " " + m[i][2] + " " + m[i][3]
+					+ " " + m[i][4] + " " + m[i][5]);
+		}
 
-                        int aux = m[k][j];
-                            m[k][j] = m[k][j - 1];
-                            m[k][j - 1] = aux;
-                    }
+	}
 
-                    else
-                        break;
-                }
-            }
-       }
-       
-       
-              
-      
-       for (int i=0; i < 3000; i++)
-       {
-         
-           System.out.print("\n Sorteio ["+(i+1)+"]:  \n "); 
-           for (int j=0; j < 6; j++)
-              
-                                         
-               System.out.printf("\n %d \n ", m[i][j]);     
-               
-                     
-       }
-                
- 
-    
-    }
-        
-    }
+}
