0bf98519249ede4e5308e3037978fd6276c339ca
Izabela Alves <Izabela Alves>
2016-11-06 19:51:57 -0200

    backup

--- src/mega/sena/Simulador.java
+++ src/mega/sena/Simulador.java
@@ -5,26 +5,34 @@ import java.util.Scanner;
 public class Simulador {
 
 	private final Scanner entrada = new Scanner(System.in);
-	private final String[] jogadores = new String[3];
+	private final String[] jogadores;
+
+    public Simulador() {
+        this.jogadores = new String[3];
+    }
 
 	public void simularTresJogadores() {
 		for (int i = 0; i < 3; i++) {
 			System.out.println("Nome do jogador(a): " + (i + 1));
 			jogadores[i] = entrada.next();
-
+                }
 			int bilhete = 0;
-			int numeros = 0;
+			int x;
+                                
 			while (bilhete < 6) {
 				for (int j = 0; j < 6;) {
 					System.out.println("Jogador(a) " + ",informe o número " + (j + 1) + ":");
 
 					try {
-						numeros = entrada.nextInt();
-						if (numeros < 1 || numeros > 60) {
+						x = entrada.nextInt();
+						if (x <= 1 ||  x <= 60) {
 							System.out.println("Digite apenas números entre 1 e 60:");
 						} else {
-							j++;
-							bilhete++;
+                                                         System.out.println("Numero incorreto, digite novamente");
+                                                         
+                                                         
+                                                         
+						
 						}
 					} catch (Exception e) {
 						System.out.println("Digite apenas números!");
@@ -35,4 +43,3 @@ public class Simulador {
 		}
 	}
         
-}
